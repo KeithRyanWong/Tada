b18ce6a013f71dec2c7bd0df248fd6156dd2b8f3
Keith Ryan Wong <KeithRyanWong@gmail.com>
2017-07-18 16:25:23 -0700

    Implement Ajax requests for frontend auth

--- frontend/tada.jsx
+++ frontend/tada.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import * as APIUtil from './util/api';
+
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Ta-da!</h1>, root);
 });
+
+window.APIUtil = APIUtil;
