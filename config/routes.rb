d22df1ee8c8966b10db9767816d143c00d0449dd
Keith Ryan Wong <KeithRyanWong@gmail.com>
2017-07-18 11:51:56 -0700

    Make Users Table

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   root "static_pages#root"
 
-  namespace :api do
+  namespace :api, defaults: {format: :json} do
     resources :users, only: [:create, :update] do
       resources :projects, only: [:index]
     end
