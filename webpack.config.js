0f6cd058018c6bb5879c7c88af32234f74cef4a8
Keith Ryan Wong <KeithRyanWong@gmail.com>
2017-07-18 09:51:30 -0700

    Setup frontend file structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-  entry: './frontend/entry.jsx',
+  entry: './frontend/tada.jsx',
   output: {
     filename: './app/assests/javascripts/bundle.js',
   },
