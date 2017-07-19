6db880525d0fcd213bd48df1512bef18a1ca7654
Keith Ryan Wong <KeithRyanWong@gmail.com>
2017-07-18 22:14:08 -0700

    Setup provider and hashrouter for react

---
+++ frontend/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <h1>Ta-Da!!</h1>
+  </div>
+);
+
+export default App;
--- frontend/tada.jsx
+++ frontend/tada.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import configureStore from './store/store';
+import Root from './components/root';
 
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-
   const store = configureStore();
+
   window.getState = store.getState;
   window.dispatch = store.dispatch;
-  
-  ReactDOM.render(<h1>Ta-da!</h1>, root);
+
+  ReactDOM.render(<Root store={store}/>, root);
 });
