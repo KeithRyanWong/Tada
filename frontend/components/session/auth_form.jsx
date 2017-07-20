06012ba8ff398c1922c9dd2595b247fe6f5e3e34
Keith Ryan Wong <KeithRyanWong@gmail.com>
2017-07-19 20:22:19 -0700

    Refactor auth components

--- frontend/components/session/auth_form.jsx
+++ frontend/components/session/auth_form.jsx
@@ -17,25 +17,102 @@ class AuthForm extends React.Component {
     this.submit = this.submit.bind(this);
   }
 
+  
+  componentDidMount() {
+    this.addClickEventToLink();
+  }
+  
+  
+  // }
+  
+  update(field) {
+    return e => {
+      e.preventDefault();
+      this.setState( {[field]: e.target.value });
+    };
+  }
+  
+  submit(e) {
+    e.preventDefault();
+    this.submitAction(this.state);
+    this.setState(this.state = { username: '',
+    first_name: '',
+    last_name: '',
+    email: '',
+    password: '',
+    image_url: ''
+  });
+}
+
+render() {
+  const {username, firstName, lastName, email, password, img_url} = this.state;
+  
+  if (this.props.action === 'Sign in') {
+    return (
+      <div>
+        <div id='sign-in' className="sign-in-link">Sign in
+          <div className="drop-down-authform-container">
+            <div className="dropdown">
+              <form className="authform">
+                <input type="text" onChange={this.update('username')} placeholder="username" value={username}/>
+                <input type="text" onChange={this.update('password')} placeholder="password" value={password}/>
+                <button className="submit">{this.props.action} </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+// return (
+  //   <div>
+  //     <button id='sign-in' className='sign-in-link'>Sign up</button>
+  //     <div className="drop-down-authform-container">
+  //       <div className="dropdown">
+  //         <form className="authform">
+  //           <input type="text" onChange={this.update('username')} placeholder="username" value={username}/>
+  //           <input type="text" onChange={this.update('firstName')} placeholder="First Name" value={firstName}/>
+  //           <input type="text" onChange={this.update('lastName')} placeholder="Last Name" value={lastName}/>
+  //           <input type="text" onChange={this.update('email')} placeholder="email" value={email}/>
+  //           <input type="text" onChange={this.update('password')} placeholder="password" value={password}/>
+  //           <input type="text" onChange={this.update('img_url')} placeholder="img_url" value={img_url}/>
+  //           <button onClick={this.submit}>{this.props.action}}</button>
+  //         </form>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
+  }
+  
   addClickEventToLink() {
-    document.getElementById("sign-in-link").addEventListener('click', (event) => {
-      if (event.target.className.includes('submit')){
-        this.submit(event);
-      }
+    document.getElementById("sign-in").addEventListener('click', (event) => {
       event.preventDefault();
       event.stopPropagation();
       let currentClass = event.currentTarget.className;
       event.currentTarget.className = 'show';
+      if (event.target.className.includes('submit')){
+        this.submit(event);
+        this.setState(this.state = { username: '',
+                                first_name: '',
+                                last_name: '',
+                                email: '',
+                                password: '',
+                                image_url: ''
+                                });
+        document.getElementById("sign-in").className = '';
+      }
     });
     
+    //handle clicking outside of form
     document.addEventListener('click', (event) => {
       event.preventDefault();
       console.log(event.target);
-      if (event.target !== document.getElementById("drop-down-authform-container") && 
-        event.target !== document.getElementById("sign-in-link")) {
+      if (event.target.className !== "drop-down-authform-container" && 
+          event.target.className !== "sign-in-link") {
         
-        document.getElementById("sign-in-link").className = '';
-  
+        document.getElementById("sign-in").className = '';
+
         this.setState(this.state = { username: '',
                                 first_name: '',
                                 last_name: '',
@@ -46,75 +123,6 @@ class AuthForm extends React.Component {
       }
     });
     
-  }
-
-  componentDidMount() {
-    this.addClickEventToLink();
-  }
-
-
-  // }
-
-  update(field) {
-    return e => {
-      e.preventDefault();
-      this.setState( {[field]: e.target.value });
-      console.log(this.state);
-    };
-  }
-
-  submit(e) {
-    console.log('hi dere');
-    e.preventDefault();
-    this.submitAction(this.state);
-    this.setState(this.state = { username: '',
-                                first_name: '',
-                                last_name: '',
-                                email: '',
-                                password: '',
-                                image_url: ''
-                                });
-  }
-
-  render() {
-    const {username, firstName, lastName, email, password, img_url} = this.state;
-
-    if (this.props.action === 'Sign in') {
-      return (
-        <div>
-          <div id='sign-in-link'>Sign in
-            <div id="drop-down-authform-container">
-              <div className="dropdown">
-                <form className="authform">
-                  <input type="text" onChange={this.update('username')} placeholder="username" value={username}/>
-                  <input type="text" onChange={this.update('password')} placeholder="password" value={password}/>
-                  <button className="submit">{this.props.action} </button>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      );
-    }
-
-    // return (
-    //   <div>
-    //     <button id='sign-in-link'>Sign up</button>
-    //     <div id="drop-down-authform-container">
-    //       <div className="dropdown">
-    //         <form className="authform">
-    //           <input type="text" onChange={this.update('username')} placeholder="username" value={username}/>
-    //           <input type="text" onChange={this.update('firstName')} placeholder="First Name" value={firstName}/>
-    //           <input type="text" onChange={this.update('lastName')} placeholder="Last Name" value={lastName}/>
-    //           <input type="text" onChange={this.update('email')} placeholder="email" value={email}/>
-    //           <input type="text" onChange={this.update('password')} placeholder="password" value={password}/>
-    //           <input type="text" onChange={this.update('img_url')} placeholder="img_url" value={img_url}/>
-    //           <button onClick={this.submit}>{this.props.action}}</button>
-    //         </form>
-    //       </div>
-    //     </div>
-    //   </div>
-    // );
   }
 }
 
