920e05cf694d92b7f2a44c2cd5431e9661bcd98e
Keith Ryan Wong <KeithRyanWong@gmail.com>
2017-07-21 16:34:54 -0700

    update sample state

--- docs/sample-state.md
+++ docs/sample-state.md
@@ -18,12 +18,14 @@
     }
   },
   projects: {
+    errors: [],
     byId: {
       1: {
+        id: 1
         title: "Cool Project",
         userId: 1,
         splashId: 12,
-        item_ids = [12, 54, 32]
+        item_order = [12, 54, 32]
         tag_ids: [1, 2, 3, 4],
         comment_ids: [23, 45, 76],
         likes: 89475,
@@ -32,10 +34,12 @@
     }
   },
   items: {
+    errors: [],
     byId: {
       1: {
+        id: 1
         projectId: 1,
-        title: "Cool Artwork",
+        source_url,
         description: "This is a cool artwork"
       }
     }
--- frontend/tada.jsx
+++ frontend/tada.jsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 
 import configureStore from './store/store';
 import Root from './components/root';
-import * as ksjd from './util/projects_api';
-import * as asd from './util/items_api';
+
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -27,15 +26,3 @@ document.addEventListener('DOMContentLoaded', () => {
 
   ReactDOM.render(<Root store={store}/>, root);
 });
-
-window.fetchProject = ksjd.fetchProject;
-window.fetchProjects = ksjd.fetchProjects;
-window.fetchUserProjects = ksjd.fetchUserProjects;
-window.submitProject = ksjd.submitProject;
-window.updateProject = ksjd.updateProject;
-
-window.fetchItem = asd.fetchItem;
-window.fetchItems = asd.fetchItems;
-window.submitItem = asd.submitItem;
-window.updateItem = asd.updateItem;
-window.deleteItem = asd.deleteItem;
