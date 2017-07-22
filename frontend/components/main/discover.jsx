aceb8ff18f5620fd0d9c0efda9d27e916e34a6b2
Keith Ryan Wong <KeithRyanWong@gmail.com>
2017-07-22 14:34:25 -0700

    'Clean commented code'

--- frontend/components/main/discover.jsx
+++ frontend/components/main/discover.jsx
@@ -2,14 +2,6 @@ import React from 'react';
 //header container
 import HeaderContainer from './header_container';
 
-//Discover will be grid-container
-// const Discover = (store) => (
-//   <div className="grid-container">
-//     <HeaderContainer store={store}/>
-//     {/* <div style={ {height: '900px' }}></div> */}
-//   </div>
-// );
-
 class Discover extends React.Component {
   constructor(props) {
     super(props);
