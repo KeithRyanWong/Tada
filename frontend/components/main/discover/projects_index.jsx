11d4ccd21cf486d8121c384ee1d4ce84a4c63f2d
Keith Ryan Wong <KeithRyanWong@gmail.com>
2017-07-24 11:00:30 -0700

    Fix autoplay

--- frontend/components/main/discover/projects_index.jsx
+++ frontend/components/main/discover/projects_index.jsx
@@ -6,16 +6,9 @@ class ProjectsIndex extends React.Component {
     this.currentUser = props.currentUser;
   }
 
-  componentDidMount() {
-    const vids = document.getElementsByClassName("splashvid");
-    Array.prototype.forEach.call(vids, (vid) => {
-      vid.play();
-    });
-  }
-
   render() {
     let splash = this.currentUser ? null : <div>
-      <video poster="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/polina.jpg" className="splashvid" autoplay loop>
+      <video poster="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/polina.jpg" className="splashvid" autoPlay loop>
         <source src="http://res.cloudinary.com/krwappacademy/video/upload/ac_none,c_scale,h_300,q_100,vc_auto,w_1920/v1500858823/loop.mp4" />
         <source src="http://res.cloudinary.com/krwappacademy/video/upload/ac_none,c_fill,h_300,q_100,vc_auto,w_1920/v1500858823/loop.webm" />
         </video>
