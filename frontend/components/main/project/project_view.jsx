3c8aa9824f515f161507d2c2ee1a523bb937c1da
Keith Ryan Wong <KeithRyanWong@gmail.com>
2017-07-24 21:28:53 -0700

    Kind of get the item index working

--- frontend/components/main/project/project_view.jsx
+++ frontend/components/main/project/project_view.jsx
@@ -9,11 +9,15 @@ class ProjectView extends React.Component {
   }
 
   render() {
-    console.log(this.items);
     return(
       <div>
         <ul>
-          
+          {this.items.map((item) => (
+            <li key={item.id} className='project-item'>
+              {item.source_type = 'png' ? <img src={`http://res.cloudinary.com/krwappacademy/image/upload/v1500849207/${item.source_url}.png`} /> : <p>This is a vid</p>}
+              <p>{item.description}</p>
+            </li>
+          ))}
         </ul>
       </div>
     );
