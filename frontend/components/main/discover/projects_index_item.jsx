2efdd21b5cdab8c33a40b4ae301f5d28f48e1e6a
Keith Ryan Wong <KeithRyanWong@gmail.com>
2017-07-24 19:11:52 -0700

    Comment out widths and heights for html, body and containers to unblock elements

--- frontend/components/main/discover/projects_index_item.jsx
+++ frontend/components/main/discover/projects_index_item.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import ExampleApp from '../project/project_modal';
 
 class ProjectsIndexItem extends React.Component {
   constructor(props) {
     super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    console.log(this.props);
   }
   
   render() {
@@ -11,6 +20,7 @@ class ProjectsIndexItem extends React.Component {
       <div className="projects-index-item">
         <img src={`http://res.cloudinary.com/krwappacademy/image/upload/c_thumb,h_200,w_200/v1500849204/${items[project.splash_id].source_url}.png`} /> 
         <span>{project.title}</span>
+        <button onClick={this.handleClick}>click!</button>
       </div>
     );
   }
