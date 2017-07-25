56949259b3b2a1fa362af75f7321fef14bb76cbe
Keith Ryan Wong <KeithRyanWong@gmail.com>
2017-07-24 19:22:32 -0700

    Open modal on project click

--- frontend/components/main/discover/projects_index_item.jsx
+++ frontend/components/main/discover/projects_index_item.jsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import ExampleApp from '../project/project_modal';
+import ProjectModal from '../project/project_modal';
 
 class ProjectsIndexItem extends React.Component {
   constructor(props) {
     super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(e) {
-    e.preventDefault();
-    e.stopPropagation();
-
-    console.log(this.props);
   }
   
   render() {
     const {items, project} = this.props;
     return (
-      <div className="projects-index-item">
-        <img src={`http://res.cloudinary.com/krwappacademy/image/upload/c_thumb,h_200,w_200/v1500849204/${items[project.splash_id].source_url}.png`} /> 
-        <span>{project.title}</span>
-        <button onClick={this.handleClick}>click!</button>
+      <div >
+        <ProjectModal project={project} items={items}/>
       </div>
     );
   }
--- frontend/components/main/project/project_modal.jsx
+++ frontend/components/main/project/project_modal.jsx
@@ -55,12 +55,13 @@ class BoardDetailModal extends React.Component {
   }
 
   render() {
+    const { items, project } = this.props;
     return(
       <div>
-        <button
-          onClick={this.openModal}>
-          <img className='index-thumbnail' src={this.props.img}></img>
-        </button>
+        <div onClick={this.openModal} className="projects-index-item">
+          <img src={`http://res.cloudinary.com/krwappacademy/image/upload/c_thumb,h_200,w_200/v1500849204/${items[project.splash_id].source_url}.png`} /> 
+          <span>{project.title}</span>
+        </div>
         <Modal
           isOpen={this.state.modalOpen}
           onAfterOpen={this.afterModalOpen}
