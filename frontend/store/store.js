100465f608c058d56de50efb13c6ee0125140ba1
Keith Ryan Wong <KeithRyanWong@gmail.com>
2017-08-16 00:39:56 -0700

    Remove duplicate webpacket and fix redux logger import

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -5,8 +5,7 @@ import thunk from 'redux-thunk';
 const middlewares = [thunk];
 
 if (process.env.NODE_ENV !== 'production') {
-  // must use 'require' (import only allowed at top of file)
-  const createLogger = require('redux-logger');
+  const createLogger = require('redux-logger').createLogger;
   middlewares.push(createLogger());
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,3 @@
-var path = require('path');
-
-module.exports = {
-  entry: './frontend/tada.jsx',
-  output: {
-    filename: './app/assets/javascripts/bundle.js',
-  },
-  module: {
-    loaders: [
-      {
-        test: [/\.jsx?$/,/\.js?$/],
-        exclude: /(node_modules)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react']
-        }
-      }
-    ]
-  },
-  devtool: 'source-map',
-  resolve: {
-    extensions: ['.js', '.jsx', '*']
-  }
-};
 
 // webpack.config.js
 var path = require("path");
