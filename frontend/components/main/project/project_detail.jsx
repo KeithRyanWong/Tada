fe11719d0e1e4652aea1b758304211fc53f99bd0
Keith Ryan Wong <KeithRyanWong@gmail.com>
2017-08-21 19:31:59 -0700

    Add icons

--- frontend/components/main/project/project_detail.jsx
+++ frontend/components/main/project/project_detail.jsx
@@ -3,13 +3,22 @@ import React from 'react';
 class ProjectDetail extends React.Component {
   render() {
     return (
-      <div className="project-detail">
-        <span>Views</span>
-        <span>comments</span>
-        <h3>{this.props.project.title}</h3>
-        By: {this.props.artist.first_name} {this.props.artist.last_name}
-        <span>star</span>
-        <span>tags</span>
+      <div className="project-detail-container">
+        <div className="project-detail">
+          <div className="project-detail-left">
+            <span><img src="http://res.cloudinary.com/krwappacademy/image/upload/c_scale,h_20/v1503368949/eye_lhddii.png"
+            />Views</span>
+            <span><img src="http://res.cloudinary.com/krwappacademy/image/upload/c_scale,h_20/v1503368949/message_n0u23n.png" />comments</span>
+          </div>
+          <div className="project-detail-middle">
+            <h3>{this.props.project.title}</h3>
+            By: {this.props.artist.first_name} {this.props.artist.last_name}
+          </div>
+          <div className="project-detail-right">
+            <span>star</span>
+            <span>tags</span>
+          </div>
+        </div>
       </div>
     );
   }
